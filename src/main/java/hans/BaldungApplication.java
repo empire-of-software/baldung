b927dfa676ef440c52a3b5bd5c5157971b1f3565
Jessica Kerr <jessitron@jessitron.com>
2018-03-15 20:12:47 -0500

    Initial commit from Atomist

---
+++ src/main/java/hans/BaldungApplication.java
@@ -0,0 +1,12 @@
+package hans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaldungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaldungApplication.class, args);
+	}
+}
---
+++ src/test/java/hans/BaldungApplicationTests.java
@@ -0,0 +1,16 @@
+package hans;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BaldungApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
